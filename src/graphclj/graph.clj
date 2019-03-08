44e14a7dfaa31b28cf01470992a67eae57b9b73d
Ghanem <marwan.t.ghanem@gmail.com>
2019-03-08 14:57:49 +0100

    first ci

---
+++ src/graphclj/graph.clj
@@ -0,0 +1,11 @@
+(ns graphclj.graph
+  (:require [clojure.string :as str]))
+
+;; Generate a graph from the lines
+(defn gen-graph [lines]
+    "Returns a hashmap contating the graph")
+
+
+(defn erdos-renyi-rnd [n,p]
+  "Returns a G_{n,p} random graph, also known as an Erdős-Rényi graph"
+)
